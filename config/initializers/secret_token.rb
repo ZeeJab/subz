cc14a1b9c3cd4315c837fe4ebb41ab55ddeb7c6c
Zahra Jabini <zahra.arch@gmail.com>
2012-11-29 15:16:36 -0500

    home cotroller, gem files, home view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subz::Application.config.secret_token = '07205fb5ecb18da9447db86b145e894df29f80855fc241ab85cc5a4f390a89562012f448338cc9d8854b69a167b44a0a93fe4803ca484a21388df98a75989eeb'
